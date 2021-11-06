f0cdc4a944a255ef926c8d9cf1aa78ecb87075f0
Kanak Prajapati <kn436428@dal.ca>
2021-11-06 20:46:39 -0300

    Working with newsapi.org API

--- platform/src/app/service/newsapiservices.service.ts
+++ platform/src/app/service/newsapiservices.service.ts
@@ -9,7 +9,7 @@ export class NewsapiservicesService {
   constructor(private _http: HttpClient) {}
 
   newsApiUrl =
-    'https://newsdata.io/api/1/news?apikey=pub_776a68f9d3447b9468fffebf0d64908c768';
+    'https://newsapi.org/v2/top-headlines?country=us&apiKey=12ce7ae5cee948039570bedda0814c8e';
 
   topHeading(): Observable<any> {
     return this._http.get(this.newsApiUrl);
